f8b07208eef3291f10e2015d577a1c40eef69996
Kees Schollaart <keesschollaart81@hotmail.com>
2015-08-06 15:42:02 +0200

    Solution geupgrade naar VS2015 + Reportviewer nu via NuGet

--- src/Website/Scripts/main.js
+++ src/Website/Scripts/main.js
@@ -1,11 +1,11 @@
-﻿/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/jquery/jquery.d.ts" />  
 /// <reference path="typings/modernizr/modernizr.d.ts" />
 /// <reference path="typings/jqueryui/jqueryui.d.ts" />
 /// <reference path="typings/custom/custom.d.ts" />
 /// <reference path="typings/jquery.cookie/jquery.cookie.d.ts" />
-/// <reference path="typings/jquery.ui.datetimepicker/jquery.ui.datetimepicker.d.ts" />
+/// <reference path="typings/jquery.ui.datetimepicker/jquery.ui.datetimepicker.d.ts" /> 
 var Prekenweb;
-(function (_Prekenweb) {
+(function (Prekenweb_1) {
     'use strict';
     var Prekenweb = (function () {
         function Prekenweb() {
@@ -30,7 +30,6 @@ var Prekenweb;
             this.preekBezochtChecksTonen = function (preekIds) {
                 if (!$.cookie('Token'))
                     return;
-
                 $.ajax({
                     type: 'GET',
                     url: prekenweb.apiRootUrl + 'api/Gebruiker/GeopendePreken',
@@ -41,7 +40,6 @@ var Prekenweb;
                     success: function (preekCookies) {
                         for (var i = 0; i < preekCookies.length; i++) {
                             var preekCookie = preekCookies[i];
-
                             if (preekCookie.DateTime != null) {
                                 // vinkje weergeven achter preek
                                 var bezochtCheckElement = $(".preek-bezocht-check-" + preekCookie.PreekId);
@@ -49,16 +47,15 @@ var Prekenweb;
                                 bezochtCheckElement.attr("title", bezochtCheckElement.attr("title") + " " + new Date(preekCookie.DateTime).toLocaleDateString());
                                 $(".datum-bezocht", bezochtCheckElement).text(new Date(preekCookie.DateTime).toLocaleDateString());
                             }
-
                             // in zoekresultaten, bladwijzer icoon actief maken
                             var bladwijzerIcoonElement = $(".preek-bladwijzer-" + preekCookie.PreekId);
                             $("span", bladwijzerIcoonElement).removeClass("fa-bookmark");
                             $("span", bladwijzerIcoonElement).removeClass("fa-bookmark-o");
-
                             if (preekCookie.BladwijzerGelegdOp == null) {
                                 $("span", bladwijzerIcoonElement).addClass("fa-bookmark-o");
                                 bladwijzerIcoonElement.data("active", "False");
-                            } else {
+                            }
+                            else {
                                 $("span", bladwijzerIcoonElement).addClass("fa-bookmark");
                                 bladwijzerIcoonElement.data("active", "True");
                             }
@@ -76,7 +73,6 @@ var Prekenweb;
                 if ($(sender).data("relid") == undefined || $(sender).data("relid") == "")
                     return;
                 var url = pw.rootUrl + pw.taal + '/Tooltip/' + $(sender).data("linktype") + '/' + $(sender).data("relid");
-
                 if ($(sender).data("preek-id") != undefined) {
                     url = url + "?preekId=" + $(sender).data("preek-id");
                 }
@@ -97,7 +93,7 @@ var Prekenweb;
                         viewport: $(window),
                         adjust: {
                             target: $(document),
-                            resize: true
+                            resize: true // Can be ommited (e.g. default behaviour)
                         }
                     },
                     show: {
@@ -111,12 +107,8 @@ var Prekenweb;
                         classes: 'qtip-tipsy qtip-shadow'
                     }
                 });
-            }).bind('click', function (event) {
-                event.preventDefault();
-                return false;
-            });
+            }).bind('click', function (event) { event.preventDefault(); return false; });
         };
-
         //
         // werden gebruikt op verschillende locaties voor inline popup weergave maar is ondertussen nergens meer in gebruik
         //
@@ -136,7 +128,7 @@ var Prekenweb;
         //            $(".ui-dialog-titlebar").hide();
         //        }
         //    });
-        //}
+        //} 
         // jquery.ajax.unobtrusive werkt nog niet met jQuery 2.0, daarom zelf even een functionaliteit gemaakt :)
         Prekenweb.prototype.unobtrusiveAjaxReplacement = function () {
             // 05-05-2015 uitgezet, zorge voor een dubbele http request op homepage
@@ -154,7 +146,6 @@ var Prekenweb;
             //    e.preventDefault();
             //});
         };
-
         //
         // TODO: was bedoelt voor als je de site toevoegd in een iPad, zodat hij niet na iedere touch naar de browser springt maar zit in de web met javascript links
         // pas weer aanzetten als javascript links wordt geexclude
@@ -190,14 +181,12 @@ var Prekenweb;
                 timeFormat: pw.taal == "nl" ? "HH:mm:ss" : "h:m:ss TT"
             });
         };
-
         Prekenweb.prototype.preekTypeCombo = function () {
             var pw = this;
             $("select[name='PreekTypeId']").each(function (index) {
                 pw.changePreekType($(this).val());
             });
         };
-
         Prekenweb.prototype.standaardTooltip = function () {
             $(".tooltip[title],.KleineIconKnop[title]").qtip({
                 position: {
@@ -210,7 +199,6 @@ var Prekenweb;
                 }
             });
         };
-
         Prekenweb.prototype.html5Placeholder = function () {
             if (!Modernizr.input.placeholder) {
                 $('[placeholder]').focus(function () {
@@ -236,36 +224,30 @@ var Prekenweb;
                 });
             }
         };
-
         Prekenweb.prototype.initializeZoeker = function () {
             var pw = this;
-
             $('html').click(function () {
                 pw.VerbergZoeker();
             });
-
             $('#zoekresultatenContainer').click(function (event) {
                 event.stopPropagation();
             });
             $("#inputZoek").bind('keyup', function (e) {
                 clearTimeout(pw.zoekerTimeout);
                 pw.zoekerTimeout = null;
-
                 if ($(this).val() == "" || e.keyCode == 27) {
                     pw.VerbergZoeker();
                     return;
                 }
                 if (e.keyCode == 13) {
                     pw.VerwerkZoekActie();
-                } else {
+                }
+                else {
                     pw.ToonZoeker();
-                    pw.zoekerTimeout = setTimeout(function () {
-                        pw.VerwerkZoekActie();
-                    }, 2500);
+                    pw.zoekerTimeout = setTimeout(function () { pw.VerwerkZoekActie(); }, 2500);
                 }
             });
         };
-
         Prekenweb.prototype.VerwerkZoekActie = function () {
             var pw = this;
             this.ToonLaderInZoeker();
@@ -292,7 +274,6 @@ var Prekenweb;
                 }
             });
         };
-
         Prekenweb.prototype.ToonZoeker = function () {
             $("#LoadingIndicator").remove();
             $("#zoekresultatenContainer").html("");
@@ -301,19 +282,16 @@ var Prekenweb;
             $("#zoekresultatenContainer").show();
             $("#container").css("opacity", "0.4");
         };
-
         Prekenweb.prototype.ToonLaderInZoeker = function () {
             var pw = this;
             $("#enterTekst").remove();
             $("#zoekresultatenContainer").append("<img src='" + pw.rootUrl + "/Content/Images/loading.gif' id='LoadingIndicator'/>");
         };
-
         Prekenweb.prototype.VerbergZoeker = function () {
             $("#driehoek").hide();
             $("#zoekresultatenContainer").hide();
             $("#container").css("opacity", "1");
         };
-
         Prekenweb.prototype.changePreekType = function (preekTypeId) {
             $(".LezingCategorieEditor").hide();
             $(".alleenBijLeespreek").hide();
@@ -330,30 +308,26 @@ var Prekenweb;
             $(".tabs").tabs();
             $("#leespreekTab").hide();
         };
-
         Prekenweb.prototype.automatischeTekstenChanged = function () {
             var value = $("input[name=AutomatischeTeksten]:checked").val();
             if (value == "True") {
                 $("#HandmatigeBijbeltekst").hide();
                 $("#AutomatischeBijbeltekst").show();
-            } else if (value == "False") {
+            }
+            else if (value == "False") {
                 $("#HandmatigeBijbeltekst").show();
                 $("#AutomatischeBijbeltekst").hide();
             }
         };
-
         Prekenweb.prototype.updateAutomatischeBijbeltekst = function () {
             var pw = this;
-
             var versVanId = $("#VersVanId").val();
             var versTotId = $("#VersTotId").val();
-
             var vanVersOmschrijving = $("#VersVanId_display").text().split(': ')[1];
             var totVersOmschrijving = $("#VersTotId_display").text().split(': ')[1];
             vanVersOmschrijving = (vanVersOmschrijving == undefined ? "" : vanVersOmschrijving);
             totVersOmschrijving = (totVersOmschrijving == undefined ? "" : " - " + totVersOmschrijving);
             $("#VersOmschrijving").val(vanVersOmschrijving + totVersOmschrijving);
-
             $("#AutomatischeBijbeltekst .value").html("Teskten ophalen...");
             $.ajax({
                 url: pw.rootUrl + pw.taal + '/Preek/Bijbelgedeelte',
@@ -366,21 +340,19 @@ var Prekenweb;
                 }
             });
         };
-
         Prekenweb.prototype.prekenWebMenu = function (show) {
             if (show == undefined)
                 show = !$("#PrekenwebTabMijn").hasClass("active");
-
             if (show) {
                 $("#zoeker").hide("fade", {}, 300);
                 $(".DefaultMenu").hide("fade", {}, 300, function () {
                     $(".MijnMenu").show("fade", {}, 300);
                 });
-
                 $("#PrekenwebTabMijn").addClass("active");
                 $("#PrekenwebTabMijn a span").removeClass("fa-chevron-down");
                 $("#PrekenwebTabMijn a span").addClass("fa-chevron-up");
-            } else {
+            }
+            else {
                 $(".MijnMenu").hide("fade", {}, 300, function () {
                     $("#zoeker").show("fade", {}, 300);
                     $(".DefaultMenu").show("fade", {}, 300);
@@ -391,11 +363,9 @@ var Prekenweb;
                 $("#PrekenwebTabMijn a span").addClass("fa-chevron-down");
             }
         };
-
         Prekenweb.prototype.toonMijnReclame = function (reden) {
             $(".prekenwebPopup").remove();
             var url = this.rootUrl + this.taal + '/PrekenWeb/UMoetInloggen?reden=' + reden;
-
             //window.open(, "Print", "height=600,width=600,scrollbars=yes,status=no,titlebar=no,toolbar=no");
             var width = window.innerWidth < 750 ? window.innerWidth - 50 : 750;
             var height = window.innerWidth < 750 ? undefined : 350;
@@ -407,7 +377,6 @@ var Prekenweb;
                 //position: [100, 100],
                 open: function () {
                     $("#InloggenMarkering").show();
-
                     $('.ui-widget-overlay').bind('click', function () {
                         $('.prekenwebPopup').dialog('destroy').remove();
                         $("#InloggenMarkering").hide();
@@ -415,7 +384,6 @@ var Prekenweb;
                     $(".ui-dialog-titlebar").hide();
                 }
             });
-
             $.ajax({
                 url: url,
                 success: function (data) {
@@ -440,7 +408,6 @@ var Prekenweb;
                     $(".ui-dialog-titlebar").hide();
                 }
             });
-
             $.ajax({
                 url: url,
                 success: function (data) {
@@ -451,41 +418,36 @@ var Prekenweb;
                 }
             });
         };
-
         Prekenweb.prototype.bladwijzerClick = function (sender, event) {
             var pw = this;
-
             event.stopPropagation();
-
             if (!pw.authenticated) {
                 pw.toonMijnReclame("Bladwijzer");
                 return false;
             }
-
             if ($(sender).data("active") == "True") {
                 pw.bladwijzerVerwijderenVanuitZoekresultaten(sender);
-            } else {
+            }
+            else {
                 pw.bladwijzerLeggenVanuitZoekresultaten(sender);
             }
             return false;
         };
-
         Prekenweb.prototype.bladWijzerMouseOver = function () {
             $(this).removeClass('fa-bookmark-o');
             $(this).addClass('fa-bookmark');
         };
-
         Prekenweb.prototype.bladWijzerMouseOut = function () {
             $(this).removeClass('fa-bookmark');
             if ($(this).parent().data("active") == "True") {
                 $(this).addClass('fa-bookmark');
-            } else {
+            }
+            else {
                 $(this).addClass('fa-bookmark-o');
             }
         };
         Prekenweb.prototype.bladwijzerLeggenVanuitZoekresultaten = function (sender) {
             var pw = this;
-
             $.ajax({
                 url: pw.rootUrl + pw.taal + '/Preek/LegBladwijzer',
                 data: { preekId: $(sender).data("preekid") },
@@ -502,10 +464,8 @@ var Prekenweb;
                 }
             });
         };
-
         Prekenweb.prototype.bladwijzerVerwijderenVanuitZoekresultaten = function (sender) {
             var pw = this;
-
             $.ajax({
                 url: pw.rootUrl + pw.taal + '/Preek/VerwijderBladwijzer',
                 data: { preekId: $(sender).data("preekid") },
@@ -525,7 +485,6 @@ var Prekenweb;
         Prekenweb.prototype.ZoekOpdrachtOpslaan = function (sender) {
             var pw = this;
             $("div", sender).addClass("fa-spin");
-
             $.ajax({
                 url: sender.href,
                 success: function () {
@@ -544,11 +503,9 @@ var Prekenweb;
         };
         return Prekenweb;
     })();
-    _Prekenweb.Prekenweb = Prekenweb;
+    Prekenweb_1.Prekenweb = Prekenweb;
 })(Prekenweb || (Prekenweb = {}));
-
 var prekenweb = new Prekenweb.Prekenweb();
-
 $(document).ready(function () {
     prekenweb.html5Placeholder();
     prekenweb.initializeZoeker();
@@ -558,7 +515,6 @@ $(document).ready(function () {
     prekenweb.unobtrusiveAjaxReplacement();
     prekenweb.dataRelatedTooltips();
     prekenweb.prekenWebMenu(prekenweb.inMijn);
-
     $(".tabs").tabs();
 });
 //# sourceMappingURL=main.js.map
