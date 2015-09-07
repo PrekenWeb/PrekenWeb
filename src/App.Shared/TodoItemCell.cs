36c1752120e2f111ca45ae0b3e19916d3af09d85
Kees Schollaart <keesschollaart81@hotmail.com>
2015-09-07 16:51:46 +0200

    Ontbrekend bestand toegevoegd

---
+++ src/App.Shared/TodoItemCell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App.Shared
+{
+    public class PreekCell : ViewCell
+    {
+        public PreekCell()
+        {
+            var label = new Label
+            {
+                YAlign = TextAlignment.Start,
+               
+            };
+            label.SetBinding(Label.TextProperty, "PreekTitel"); 
+
+            var layout = new StackLayout
+            {
+                Padding = new Thickness(20, 20, 0, 0),
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                Children = { label/*, tick*/ }
+            }; 
+            View = layout;
+        }
+
+        protected override void OnBindingContextChanged()
+        { 
+            View.BindingContext = BindingContext;
+            base.OnBindingContextChanged();
+        }
+    }
+}
