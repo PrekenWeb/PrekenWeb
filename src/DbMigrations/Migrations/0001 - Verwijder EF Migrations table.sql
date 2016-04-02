dcf304d54904cc8b0f9774c52c3e9ec4f6000fc8
Kees Schollaart <keesschollaart81@hotmail.com>
2016-04-02 13:56:16 +0200

    Test data wanneer database leeg is

--- src/DbMigrations/Migrations/0001 - Verwijder EF Migrations table.sql
+++ src/DbMigrations/Migrations/0001 - Verwijder EF Migrations table.sql
@@ -1,2 +1,5 @@
-﻿drop table [dbo].[__MigrationHistory]
-drop table [dbo].[__RefactorLog]
+﻿IF OBJECT_ID(N'dbo.__MigrationHistory', N'U') IS NOT NULL
+BEGIN
+	drop table [dbo].[__MigrationHistory]
+	drop table [dbo].[__RefactorLog]
+END
