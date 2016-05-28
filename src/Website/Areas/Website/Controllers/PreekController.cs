a2cd942c0ed80b0e5d015f4d6c67863e2761a983
Kees Schollaart <keesschollaart81@hotmail.com>
2016-05-28 17:36:39 +0200

    Revert "Dynamische preek locatie"
    This reverts commit 5c6e0d3b07fbaf696e496b9baab5686dc21fd7e4.

--- src/Website/Areas/Website/Controllers/PreekController.cs
+++ src/Website/Areas/Website/Controllers/PreekController.cs
@@ -23,7 +23,6 @@ using System.Web;
 using System.Web.Mvc;
 using VikingErik.Mvc.ResumingActionResults;
 using ContentDisposition = System.Net.Mime.ContentDisposition;
-using System.Configuration;
 
 namespace Prekenweb.Website.Areas.Website.Controllers
 {
@@ -192,12 +191,12 @@ namespace Prekenweb.Website.Areas.Website.Controllers
                 }
             }
             if (inline.Value)
-            {  
-                return new ResumingFilePathResult(string.Format("{0}{1}", ConfigurationManager.AppSettings["PrekenFolder"], preek.Bestandsnaam), preek.GetContentType());
+            {
+                return new ResumingFilePathResult(Server.MapPath("~/Content/Preken/" + preek.Bestandsnaam), preek.GetContentType());
             }
 
             Response.AppendHeader("Content-Disposition", new ContentDisposition { FileName = preek.Bestandsnaam, Inline = false }.ToString());
-            return File(string.Format("{0}{1}", ConfigurationManager.AppSettings["PrekenFolder"], preek.Bestandsnaam), preek.GetContentType());
+            return File("~/Content/Preken/" + preek.Bestandsnaam, preek.GetContentType());
 
         }
 
