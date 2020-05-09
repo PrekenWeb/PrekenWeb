6f53ec31364f02cce3380f7efdafc51e6e545819
alievense <andrelievense@gmail.com>
2020-05-09 14:36:50 +0200

    Rename youtube > video

---
+++ src/Data.Migrations/Migrations/0006 - Rename Preek_Youtube_Video.sql
@@ -0,0 +1,7 @@
+﻿IF EXISTS (SELECT column_name 
+		   FROM INFORMATION_SCHEMA.columns 
+		   WHERE table_name = 'Preek' 
+		   AND column_name = 'Youtube')
+BEGIN
+	EXEC sp_rename 'dbo.Preek.Youtube', 'Video', 'COLUMN';
+END
