59d10df66e6c08084c7d025110720b7a886d42bf
alievense <andrelievense@gmail.com>
2020-06-27 10:03:41 +0200

    Captcha upgrade 4.0.1 > 4.4.2

--- src/Website/App_Start/RouteConfig.cs
+++ src/Website/App_Start/RouteConfig.cs
@@ -1,12 +1,7 @@
-﻿using CaptchaMvc.Infrastructure;
-using CaptchaMvc.Interface;
-using System.Collections.Generic;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace Prekenweb.Website
+﻿namespace Prekenweb.Website
 {
-    using CaptchaMvc.Controllers;
+    using System.Web.Mvc;
+    using System.Web.Routing;
 
     public class RouteConfig
     {
@@ -18,30 +13,13 @@ namespace Prekenweb.Website
             // BotDetect requests must not be routed
             routes.IgnoreRoute("{*botdetect}", new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
 
-            ////routes.MapRoute("CaptchaRefresh", "DefaultCaptcha/Refresh", new { controller = "DefaultCaptcha", action = "Refresh" });
-            ////routes.MapRoute("CaptchaImage", "DefaultCaptcha/Generate", new { controller = "DefaultCaptcha", action = "Generate" });
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { id = UrlParameter.Optional }
             );
 
-            //var defaultCaptchaManager = (DefaultCaptchaManager)CaptchaUtils.CaptchaManager;
-            //defaultCaptchaManager.ImageUrlFactory = (helper, pair) => ImageUrlFactory(defaultCaptchaManager, helper, pair);
-            //defaultCaptchaManager.RefreshUrlFactory = RefreshUrlFactory; 
-
             routes.MapMvcAttributeRoutes();
         }
-
-        //private static string RefreshUrlFactory(UrlHelper urlHelper, KeyValuePair<string, ICaptchaValue> keyValuePair)
-        //{
-        //    return urlHelper.RouteUrl("CaptchaRefresh");
-        //}
-
-        //private static string ImageUrlFactory(DefaultCaptchaManager captchaManager, UrlHelper urlHelper, KeyValuePair<string, ICaptchaValue> keyValuePair)
-        //{
-        //    return urlHelper.RouteUrl("CaptchaImage", new RouteValueDictionary { { captchaManager.TokenParameterName, keyValuePair.Key } });
-        //} 
-    } 
+    }
 }
