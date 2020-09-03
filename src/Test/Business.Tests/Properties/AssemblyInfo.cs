44c3e48501ea188b9319aa4d5997b6a15e2b4be5
alievense <andrelievense@gmail.com>
2020-09-03 20:40:57 +0200

    Upgrade Business.Tests.csproj

--- src/Test/Business.Tests/Properties/AssemblyInfo.cs
+++ src/Test/Business.Tests/Properties/AssemblyInfo.cs
@@ -1,24 +1,6 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+
 using Castle.Core.Internal;
-
-[assembly: AssemblyTitle("Business.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Business.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("d84a4224-7219-4a66-9ef0-dfb39edd99b9")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Make internals visible to Moq.
 [assembly: InternalsVisibleTo(InternalsVisible.ToCastleCore)]
