3f5742423958d8fa5fc063f53149b4716eaad424
André Lievense <andre.lievense@gmail.com>
2023-11-07 21:54:37 +0100

    ANBI-status pagina toegevoegd

--- src/Resources/Resources.Designer.cs
+++ src/Resources/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Prekenweb.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -255,6 +255,15 @@ namespace Prekenweb.Resources {
         public static string AllLecturesHaveBeenPublished {
             get {
                 return ResourceManager.GetString("AllLecturesHaveBeenPublished", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ANBI-status.
+        /// </summary>
+        public static string AnbiStatus {
+            get {
+                return ResourceManager.GetString("AnbiStatus", resourceCulture);
             }
         }
         
